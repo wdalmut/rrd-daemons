2cb7b9b757fadaef5287bac39ce79a13c0cb116f
Walter Dal Mut <walter.dalmut@gmail.com>
2012-05-29 19:38:05 +0200

    add base example

---
+++ Eg.php
@@ -0,0 +1,23 @@
+<?php 
+class Eg 
+    extends Core_Daemon
+{
+    protected function setup()
+    {
+        
+    }
+    
+    protected function execute()
+    {
+        echo "OK" . PHP_EOL;
+    } 
+    
+    protected function log_file()
+    {
+        $dir = __DIR__ . '/daemon';
+        if (@file_exists($dir) == false)
+            @mkdir($dir, 0777, true);
+        
+        return $dir . '/log_' . date('Ymd');
+    }
+}
